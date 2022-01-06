dd6d24f860f71068414c59dd840e16fb16b1c988
abdulbariks <abdulbarik1997m@gmail.com>
2022-01-06 15:03:12 +0600

    while lop in php

--- index.php
+++ index.php
@@ -8,9 +8,12 @@
 </head>
 <body>
     <?php
-           for($i = 1 ; $i <= 10 ; $i++){
-               echo $i ."<br>";
-           }
+          $i = 1;
+          while($i <= 10){
+              echo $i . "<br>";
+              $i++;
+          }
+          
     ?>
 </body>
 </html>
